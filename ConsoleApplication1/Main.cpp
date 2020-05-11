4cc51c194200951be5382daec66774424087f0c1
modori <modori@innosim.com>
2020-05-11 13:40:39 +0900

    Add 함수 추가

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -4,9 +4,17 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+
+int add(int _nInputA, int _nInputB);
+
 int main()
 {
 	cout << "Hello world" << endl;
 
 	return 0;
+}
+
+int add(int _nInputA, int _nInputB)
+{
+	return _nInputA * _nInputB;
 }
