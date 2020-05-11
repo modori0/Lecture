984d64873b61d5c8b77096a2182535ecee2ea5a3
modori <modori@innosim.com>
2020-05-11 14:17:59 +0900

    multiple

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -6,6 +6,7 @@ using std::endl;
 
 
 int add(int _nInputA, int _nInputB);
+int multiple(int _nInputA, int _nInputB);
 
 int main()
 {
@@ -15,6 +16,11 @@ int main()
 }
 
 int add(int _nInputA, int _nInputB)
+{
+	return _nInputA * _nInputB;
+}
+
+int multiple(int _nInputA, int _nInputB)
 {
 	return _nInputA * _nInputB;
 }
